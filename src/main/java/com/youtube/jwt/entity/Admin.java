5072bbe65c7a344a26d830547db19a9023a3ee11
sharmarohit45 <officialrishi45@gmail.com>
2024-07-15 11:12:25 +0530

    updated code

--- src/main/java/com/youtube/jwt/entity/Admin.java
+++ src/main/java/com/youtube/jwt/entity/Admin.java
@@ -1,63 +1,75 @@
 package com.youtube.jwt.entity;
-import javax.persistence.*;
 
+import javax.persistence.*;
 
 @Entity
 public class Admin {
-	
-	 @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private Long adminId;
-	    private String adminName;
-	    private String email;
-	    private String password;
-	    private String country;
-	    private String mobileNo;
-	    private String gender;
-	    private String role;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long adminId;
+	private String adminName;
+	private String email;
+	private String password;
+	private String country;
+	private String mobileNo;
+	private String gender;
+	private String role;
+
 	public String getAdminName() {
 		return adminName;
 	}
+
 	public void setAdminName(String adminName) {
 		this.adminName = adminName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
 	public String getMobileNo() {
 		return mobileNo;
 	}
+
 	public void setMobileNo(String mobileNo) {
 		this.mobileNo = mobileNo;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public String getRole() {
 		return role;
 	}
+
 	public void setRole(String role) {
 		this.role = role;
 	}
-	 
-	 
 
 }
